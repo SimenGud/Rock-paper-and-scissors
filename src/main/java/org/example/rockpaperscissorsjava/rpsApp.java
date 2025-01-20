0784c2f2688c07f3a1990d0068f6f2b479da2779
SimenGud <simeng2003@gmail.com>
2025-01-20 10:16:08 +0100

    Added Constructor

---
+++ src/main/java/org/example/rockpaperscissorsjava/rpsApp.java
@@ -0,0 +1,32 @@
+package org.example.rockpaperscissorsjava;
+
+public class rpsApp
+
+{
+  String Rock = "Rock";
+  String Paper = "Paper";
+  String Scissors = "Scissors";
+
+  public rpsApp()
+  {
+    this.Rock = Rock;
+    this.Paper = Paper;
+    this.Scissors = Scissors;
+  }
+
+  public void setRock(String Rock)
+  {
+    this.Rock = Rock;
+  }
+
+  public void setPaper(String Paper)
+  {
+    this.Paper = Paper;
+  }
+
+  public void setScissors(String Scissors)
+  {
+    this.Scissors = Scissors;
+  }
+
+}
