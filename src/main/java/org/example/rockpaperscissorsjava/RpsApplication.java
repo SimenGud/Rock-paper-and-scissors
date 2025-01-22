dea6030f3d89021fb77cbb0208225703bd48452e
SimenGud <simeng2003@gmail.com>
2025-01-22 10:06:58 +0100

    Refactored default files used as references

--- src/main/java/org/example/rockpaperscissorsjava/RpsApplication.java
+++ src/main/java/org/example/rockpaperscissorsjava/RpsApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class RpsApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(RpsApplication.class.getResource("Rps-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 400, 300);
     stage.setTitle("Rock, Paper, Scissors");
     stage.setScene(scene);
--- src/main/java/org/example/rockpaperscissorsjava/RpsController.java
+++ src/main/java/org/example/rockpaperscissorsjava/RpsController.java
@@ -3,7 +3,7 @@ package org.example.rockpaperscissorsjava;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RpsController {
   @FXML
   private Label resultLabel;
 
