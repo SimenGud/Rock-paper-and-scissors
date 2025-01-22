a238b003105cfad996b886aade8653b23c861658
SimenGud <simeng2003@gmail.com>
2025-01-22 09:37:20 +0100

    Added replay feature

--- src/main/java/org/example/rockpaperscissorsjava/RpsApp.java
+++ src/main/java/org/example/rockpaperscissorsjava/RpsApp.java
@@ -15,6 +15,33 @@ public class RpsApp {
     System.out.println("Welcome to Rock, Paper, Scissors!");
     System.out.println("Please enter your choice: Rock, Paper, or Scissors");
     String userChoice = scanner.nextLine();
+    computerThrow();
+    System.out.println("Computer choice: " + computerChoice);
+    String result = determineWinner(userChoice, computerChoice);
+    System.out.println(result);
+    System.out.println("Want to play again? (Y/N)");
+    String playAgain = scanner.nextLine();
+    if (playAgain.equals("Y")) {
+      run();
+    } else {
+      System.out.println("Thanks for playing!");
+    }
+  }
+
+  private String determineWinner(String userChoice, String computerChoice) {
+    if (userChoice.equals(computerChoice)) {
+      return tie;
+    } else if (
+        userChoice.equals("Rock") && computerChoice.equals("Scissors") ||
+            (userChoice.equals("Paper") && computerChoice.equals("Rock")) ||
+            (userChoice.equals("Scissors") && computerChoice.equals("Paper"))) {
+      return victory;
+    } else {
+      return defeat;
+    }
+  }
+
+  private String computerThrow() {
     if (RpsValues.generateComputerChoice().equals("Rock")) {
       computerChoice = "Rock";
     } else if (RpsValues.generateComputerChoice().equals("Paper")) {
@@ -22,22 +49,6 @@ public class RpsApp {
     } else {
       computerChoice = "Scissors";
     }
-    System.out.println("Computer choice: " + computerChoice);
-    String result = determineWinner(userChoice, computerChoice);
-    System.out.println(result);
-  }
-
-  private String determineWinner(String userChoice, String computerChoice) {
-    if (userChoice.equals(computerChoice)) {
-      return tie;
-    } else if (userChoice.equals("Rock") && computerChoice.equals("Scissors")) {
-      return victory;
-    } else if (userChoice.equals("Paper") && computerChoice.equals("Rock")) {
-      return victory;
-    } else if (userChoice.equals("Scissors") && computerChoice.equals("Paper")) {
-      return victory;
-    } else {
-      return defeat;
-    }
+    return computerChoice;
   }
 }
